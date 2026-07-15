9d2051a04d27605bb11a648aaa92aadb86d3c428
Mohammed Tarief <tarief250@gmail.com>
2026-07-15 20:04:33 +0530

    Time: 24 ms (50.08%) | Memory: 76.3 MB (80.75%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+      Set<Integer>set  = new HashSet<Integer>();
+      for(int i =0;i<=nums.length-1;i++){
+        if(set.contains(nums[i]))
+        return true;
+        set.add(nums[i]);
+        if(set.size()>k){
+            set.remove(nums[i-k]);
+        }
+      }return false;
+
+    }
+}

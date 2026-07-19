30d7bf312e0a7ab45a02663eefebe1c25ccae01d
Mohammed Tarief <tarief250@gmail.com>
2026-07-19 23:00:56 +0530

    Time: 5 ms (80.65%) | Memory: 45.9 MB (53.67%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+
+    
+        if (x < 0) return false;
+
+        int cp = x;
+        int res = 0;   
+
+        while (x != 0) {
+            int rem = x % 10;
+            res = res * 10+rem;
+            x = x / 10;
+        }
+
+        return cp == res;
+    }
+}
